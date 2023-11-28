3f956ec2c9017395fd83d4af2dd36a641bb1c453
KhanhJapan <khanhbeobu2@gmail.com>
2023-11-29 02:49:53 +0700

    upload file to S3

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = "AKIA4OAUHYK6SFWSMREB"
-  secret_key = "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"
+  access_key = "AKIA4OAUHYK64FVVVBY4"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "khanhnx3_bucket" {
   bucket = "khanhnx3-bucket"
+}
+
+resource "aws_s3_bucket_object" "file_upload" {
+  bucket = aws_s3_bucket.khanhnx3_bucket.id
+  key = "file_test.txt"
+  source = "D://VinFast/AWS_Exercise/terraform/Aws_Exercise/file_test.txt"
 }
